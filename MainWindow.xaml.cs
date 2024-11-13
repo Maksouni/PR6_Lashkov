ec2ab697bf781ce3cf8488f6213f41f0587b5037
Maksouni <max1205325@gmail.com>
2024-11-13 12:55:47 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using PR6_Lashkov.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR6_Lashkov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            fr_content.Content = new Auth();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            fr_content.GoBack();
+        }
+
+        private void frame_Content(object sender, EventArgs e)
+        {
+            if (fr_content.CanGoBack)
+                btnBack.Visibility = Visibility.Visible;
+            else
+                btnBack.Visibility = Visibility.Hidden;
+
+        }
+    }
+}
