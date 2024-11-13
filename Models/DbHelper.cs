10038b8ce1e8cbd9fa3e0e24bb28776c7026abbf
Maksouni <max1205325@gmail.com>
2024-11-13 15:23:39 +0700

    done

---
+++ Models/DbHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR6_Lashkov.Models
+{
+    internal class DbHelper
+    {
+        private static DrinkFactoryEntities _context;
+
+        public static DrinkFactoryEntities GetContext()
+        {
+            if (_context == null)
+                _context = new DrinkFactoryEntities();
+            return _context;
+        }
+
+        public static void CreateUser(Users user)
+        {
+            var context = GetContext();
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+    }
+}
--- Pages/Auth.xaml.cs
+++ Pages/Auth.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using PR6_Lashkov.Models;
+using PR6_Lashkov.Services;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,19 +23,93 @@ namespace PR6_Lashkov.Pages
     /// </summary>
     public partial class Auth : Page
     {
+        int click;
         public Auth()
         {
             InitializeComponent();
+            tbCaptcha.Visibility = Visibility.Collapsed;
+            tblCaptcha.Visibility = Visibility.Collapsed;
+            click = 0;
         }
 
         private void btnEnterGuest_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new Client(null));
         }
+
+        private void GenerateCapctcha()
+        {
+            tbCaptcha.Visibility = Visibility.Visible;
+            tblCaptcha.Visibility = Visibility.Visible;
+
+            string capctchaText = CaptchaGenerator.GenerateCaptchaText(6);
+            tblCaptcha.Text = capctchaText;
+            tblCaptcha.TextDecorations = TextDecorations.Strikethrough;
+        }
+
 
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
+            click += 1;
+            string login = tbLogin.Text.Trim();
+            string password = Hash.HashPassword(tbPassword.Text.Trim());
+
+            DrinkFactoryEntities db = DbHelper.GetContext();
+
+            var user = db.Users.Where(x => x.login == login && x.password == password).FirstOrDefault();
+            if (click == 1)
+            {
+                if (user != null)
+                {
+                    MessageBox.Show("Вы вошли под: " + user.Roles.name.ToString());
+                    LoadPage(user);
+                }
+                else
+                {
+                    MessageBox.Show("Вы ввели логин или пароль неверно!");
+                    GenerateCapctcha();
+                    tbPassword.Text = String.Empty;
+                }
+            }
+            else if (click > 1)
+            {
+                if (user != null && tbCaptcha.Text == tblCaptcha.Text)
+                {
+                    MessageBox.Show("Вы вошли под: " + user.Roles.name.ToString());
+                    LoadPage(user);
+                }
+                else
+                {
+                    MessageBox.Show("Введите данные заново!");
+                }
+            }
+            
 
         }
+
+        private void LoadPage(Users user)
+        {
+            click = 0;
+            tbCaptcha.Visibility = Visibility.Collapsed;
+            tblCaptcha.Visibility = Visibility.Collapsed;
+            tbCaptcha.Text = "";
+            tbPassword.Text = "";
+            tbLogin.Text = "";
+
+            switch (user.Roles.name)
+            {
+                case "customer":
+                    NavigationService.Navigate(new Client(user));
+                    break;
+                case "admin":
+                    NavigationService.Navigate(new Admin(user));
+                    break;
+                case "employee":
+                    NavigationService.Navigate(new Employee(user));
+                    break;
+            }
+
+        }
+
     }
 }
