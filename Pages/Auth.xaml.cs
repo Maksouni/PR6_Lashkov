1392876b3ce4baba1738ed9bb8c4c28bfc8add5c
Maksouni <max1205325@gmail.com>
2024-11-25 13:59:13 +0700

    PZ7

--- Pages/Auth.xaml.cs
+++ Pages/Auth.xaml.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PR6_Lashkov.Pages
 {
@@ -24,11 +26,15 @@ namespace PR6_Lashkov.Pages
     public partial class Auth : Page
     {
         int click;
+        Timer timer;
+        int seconds;
         public Auth()
         {
             InitializeComponent();
             tbCaptcha.Visibility = Visibility.Collapsed;
             tblCaptcha.Visibility = Visibility.Collapsed;
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
             click = 0;
         }
 
@@ -82,10 +88,46 @@ namespace PR6_Lashkov.Pages
                 {
                     MessageBox.Show("Введите данные заново!");
                     GenerateCapctcha();
+                    if (click >= 3)
+                    {
+                        btnEnter.IsEnabled = false;
+                        btnEnterGuest.IsEnabled = false;
+                        tbLogin.IsEnabled = false;
+                        tbPassword.IsEnabled = false;
+                        tbCaptcha.IsEnabled = false;
+                        seconds = 0;
+                        timer.Start();
+                    }
                 }
             }
-            
+        }
 
+        public void Timer_Elapsed(object sender, ElapsedEventArgs elapsed)
+        {
+            if (seconds < 10)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    tbTimer.Text = $"Повторите попытку через {10 - seconds}";
+                });
+            }
+            else
+            {
+                timer.Stop();
+                Dispatcher.Invoke(() =>
+                {
+                    btnEnter.IsEnabled = true;
+                    btnEnterGuest.IsEnabled = true;
+                    tbLogin.IsEnabled = true;
+                    tbPassword.IsEnabled = true;
+                    tbCaptcha.IsEnabled = true;
+                    click = 1;
+                    tbTimer.Text = String.Empty;
+                });
+            }
+            seconds++;
+            
+                
         }
 
         private void LoadPage(Users user)
